8f716e17e8e55c3a14bb4581722f5fa8c19f6aed
Mario Cecchi <macecchi@gmail.com>
2015-06-22 20:01:35 -0300

    Faz a leitura automática do status num intervalo

--- app.js
+++ app.js
@@ -2,17 +2,25 @@ var express = require('express'),
     app = express(),
     request = require('request');
 
+var loadTimeout;
+var interval = 15*1000;
 var IPPlugMeter = '10.10.11.144';
 var rede = 127.0;
 var precokWh = 0.71;
+var status;
 
  app.get('/custo_estimado', function(req, res){
  	console.log("REQUEST: " + req.method + " " + req.url);
     request('http://' + IPPlugMeter + '/current', function (error, response, body) {
 	  if (!error && response.statusCode == 200) {
 	  	var current = parseFloat(body);
-	  	var potencia = rede * current;
-	  	var custo_estimado = (potencia / 1000) * precokWh;
+	  	var custo_estimado = 0;
+	  	if (!isNaN(current)) {
+	  		var potencia = rede * current;
+	  		custo_estimado = (potencia / 1000) * precokWh;
+	  	} else {
+	  		custo_estimado = "ERRO LEITURA";
+	  	}
 	    res.send("" + custo_estimado);
 	  }
 	  else if (!error && response.statusCode != 200) {
@@ -54,5 +62,21 @@ var precokWh = 0.71;
 	});
   });
 
+ function getStatus() {
+ 	request('http://' + IPPlugMeter + "/", function (error, response, body) {
+    	clearInterval(loadTimeout);
+ 		if (!error && response.statusCode == 200) {
+	    	status = JSON.parse(body);
+	    	console.log(status.corrente);
+	    	loadTimeout = setTimeout(function(){ 
+	    		getStatus();
+	    	}, interval);
+		} else {
+			getStatus();
+		} 	    
+    });
+ }
+
 var server = app.listen(80);
 console.log('Servidor Express iniciado na porta %s', server.address().port);
+getStatus();
