ed7ffa83abdc5ca0b2654106fd5bba0ad0024712
Mario Cecchi <macecchi@gmail.com>
2015-06-22 18:55:21 -0300

    Faz cálculo do custo estimado e melhor tratamento de erro

--- app.js
+++ app.js
@@ -2,21 +2,54 @@ var express = require('express'),
     app = express(),
     request = require('request');
 
+var IPPlugMeter = '10.10.11.144';
+var rede = 127.0;
+var precokWh = 0.71;
+
+ app.get('/custo_estimado', function(req, res){
+ 	console.log("REQUEST: " + req.method + " " + req.url);
+    request('http://' + IPPlugMeter + '/current', function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	  	var current = parseFloat(body);
+	  	var potencia = rede * current;
+	  	var custo_estimado = (potencia / 1000) * precokWh;
+	    res.send("" + custo_estimado);
+	  }
+	  else if (!error && response.statusCode != 200) {
+	  	res.send('Error ' + response.statusCode);
+	  }
+	  else {
+	  	res.send('Error' + error);
+	  }
+	});
+  });
 
  app.get('/*', function(req, res){
  	console.log("REQUEST: " + req.method + " " + req.url);
-    request('http://10.10.11.144' + req.url, function (error, response, body) {
+    request('http://' + IPPlugMeter + req.url, function (error, response, body) {
 	  if (!error && response.statusCode == 200) {
 	    res.send(body); 
+	  }
+	  else if (!error && response.statusCode != 200) {
+	  	res.send('Error ' + response.statusCode);
+	  }
+	  else {
+	  	res.send('Error ' + error);
 	  }
 	});
   });
 
  app.post('/*', function(req, res){
  	console.log("REQUEST: " + req.method + " " + req.url);
-    request.post('http://10.10.11.144' + req.url, function (error, response, body) {
+    request.post('http://' + IPPlugMeter + req.url, function (error, response, body) {
 	  if (!error && response.statusCode == 200) {
 	    res.send(body); 
+	  }
+	  else if (!error && response.statusCode != 200) {
+	  	res.send('Error ' + response.statusCode);
+	  }
+	  else {
+	  	res.send('Error ' + error);
 	  }
 	});
   });
