7cf148d96d739d4ad4df3bde1764b03479b63419
Mario Cecchi <macecchi@gmail.com>
2015-06-22 18:16:48 -0300

    Roteia todas as requests de GET e POST pro ip do meter

--- app.js
+++ app.js
@@ -3,14 +3,23 @@ var express = require('express'),
     request = require('request');
 
 
- app.get('/', function(req, res){
-    request('http://www.google.com', function (error, response, body) {
+ app.get('/*', function(req, res){
+ 	console.log("REQUEST: " + req.method + " " + req.url);
+    request('http://10.10.11.144' + req.url, function (error, response, body) {
 	  if (!error && response.statusCode == 200) {
 	    res.send(body); 
 	  }
 	});
   });
 
+ app.post('/*', function(req, res){
+ 	console.log("REQUEST: " + req.method + " " + req.url);
+    request.post('http://10.10.11.144' + req.url, function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	    res.send(body); 
+	  }
+	});
+  });
 
 var server = app.listen(80);
 console.log('Servidor Express iniciado na porta %s', server.address().port);
